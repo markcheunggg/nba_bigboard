2899a49700586bd02c0531c1d9713d171d4c0824
Arunan Thiviyanathan <arunankogulan@gmail.com>
2021-05-19 17:32:47 -0500

    Added Sidebar

--- index.js
+++ index.js
@@ -32,16 +32,26 @@ function nameSearch(){
 function create_modal(id) {
     var stats = data[id]
     console.log(stats);
-
-    document.getElementById('modal-text').innerHTML = `            
-    <img src='img/${id+1}.png' style="float: right; height: 200px;" >
-    <h1>${stats.Player}</h1>
+    document.getElementById("name").innerHTML = `${stats.Player}`;
+    document.getElementById("team").innerHTML = `${stats.Team}`;
+    var img = document.createElement("img");
+    img.src = `img/${id+1}.png`;
+    img.style = "float: right; height: 200px;";
+    document.getElementById("name").insertBefore(img, null);
+    document.getElementById("stats").innerHTML = 
+    `Points Per Game: ${stats.PTS}<br><br>
+    Assists Per Game: ${stats.AST}<br><br>
+    Rebounds Per Game: ${stats.TRB}<br><br>
+    Steals Per game: ${stats.STL}<br><br>
+    Blocks Per Game: ${stats.BLK}<br><br>`;
+    /*document.getElementById('modal-text').innerHTML = `            
     <h2 style="padding-top:0">Team: ${stats.Team}</h2>
     Points Per Game: ${stats.PTS}<br><br>
     Assists Per Game: ${stats.AST}<br><br>
     Rebounds Per Game: ${stats.TRB}<br><br>
     Steals Per game: ${stats.STL}<br><br>
-    Blocks Per Game: ${stats.BLK}<br><br>`;
-
-    modal.style.display = "block";
-}
+    Blocks Per Game: ${stats.BLK}<br><br>`;*/
+    document.getElementById("mySidebar").style.width = "250px";
+    //document.getElementById("bottom-bar").style.marginLeft = "250px";
+            // ^ This shifts the bottom box to the left when the sidebar is opened
+}    
